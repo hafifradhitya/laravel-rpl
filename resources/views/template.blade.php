bc38ca57ea080d81fd55031428c6e011056f61b3
hafifradhitya <bangclasher81@gmail.com>
2024-07-05 20:28:29 +0700

    Update views/template.blade.php
    mengubah judul/title

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>AplikasiKu</title>
+    <title>AplikasiKu By RadhityaHafif</title>
     <link href="{{ url('bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css">
 </head>
 <body data-bs-theme="dark">
